428fbc13ef87180c5030f1251c97672c3b2abc1a
vmqxdv <vquintilio@gmail.com>
2025-02-27 15:04:18 +0100

    feat: Dichiarata costante "validEmails"

---
+++ mail/script.js
@@ -0,0 +1,27 @@
+/*
+ 
+  Mail
+  
+  - Crea una lista di email di invitati ad una festa.
+  - Chiedi all’utente la sua email, controlla che sia 
+  nella lista di chi può accedere, stampa un 
+  messaggio appropriato sull’esito del controllo, 
+  utilizzando un ciclo for.
+  
+  Non è consentito usare nessun metodo proprio degli 
+  array (come includes, per esempio).
+  Si può fare? Certo che si basta ragionare un po’.
+  Nota:
+  Non è necessario provvedere alla validazione delle email
+
+ */
+
+
+const validEmails = [
+  'valerioquintilio@gmail.com',
+  'albertocadorin@hotmail.it',
+  'marcorocco@yahoo.com',
+  'michelangelomusco@libero.it',
+  'valeriodibiagio@gmail.com',
+];
+
