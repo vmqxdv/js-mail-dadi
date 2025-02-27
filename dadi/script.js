d7bf8106f63fb6db7ae37acd715068b7580bf19e
vmqxdv <vquintilio@gmail.com>
2025-02-27 15:58:42 +0100

    feat: Creato prompt per richiedere username

---
+++ dadi/script.js
@@ -0,0 +1,15 @@
+/*
+
+  Gioco dei dadi
+  Generare un numero random da 1 a 6, sia per il giocatore
+  sia per il computer.
+  Stabilire il vincitore, in base a chi fa il punteggio più alto.
+  
+  Bonus
+  Invece di generare il numero random per l'utente, chiedilo
+  con un prompt!
+
+*/
+
+
+const userName = prompt('Inserisci un username:');
